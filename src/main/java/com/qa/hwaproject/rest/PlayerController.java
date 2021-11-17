46c03f77f7d22da119ce50560ff0704a8eb02210
unknown <alzein7@outlook.com>
2021-11-17 20:42:37 +0000

    updated the player controller

--- src/main/java/com/qa/hwaproject/rest/PlayerController.java
+++ src/main/java/com/qa/hwaproject/rest/PlayerController.java
@@ -12,12 +12,6 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 import com.qa.hwaproject.persistence.domain.Player;
 
 @RestController
