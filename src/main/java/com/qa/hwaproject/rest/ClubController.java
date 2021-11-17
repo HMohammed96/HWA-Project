76ec9c58f3bd175abff96dbe17565f732429a87a
unknown <alzein7@outlook.com>
2021-11-17 16:47:26 +0000

    HWA-13 #comment this is my club controller java file added with the create method included

---
+++ src/main/java/com/qa/hwaproject/rest/ClubController.java
@@ -0,0 +1,33 @@
+package com.qa.hwaproject.rest;
+
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.qa.hwaproject.persistence.domain.Club;
+
+@RestController
+@RequestMapping("/club")
+public class ClubController {
+
+	private ClubService service;
+
+	public ClubController(ClubService service) {
+		this.service = service;
+	}
+
+	// CREATE
+	@PostMapping("/create")
+	public ResponseEntity<Club> addClub(@RequestBody Club club) {
+		return new ResponseEntity<Club>(this.service.addClub(club), HttpStatus.CREATED);
+	}
+}
