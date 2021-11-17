45ac6ea925bef23ccbcb676e986514bdc43e35d4
unknown <alzein7@outlook.com>
2021-11-17 17:18:58 +0000

    HWA-1 #comment i created my club repo java file

--- src/main/java/com/qa/hwaproject/persistence/repos/PlayerRepo.java
+++ src/main/java/com/qa/hwaproject/persistence/repos/PlayerRepo.java
@@ -1,5 +1,9 @@
 package com.qa.hwaproject.persistence.repos;
 
-public class PlayerRepo {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.qa.hwaproject.persistence.domain.Player;
+
+public interface PlayerRepo extends JpaRepository<Player, Long> {
 
 }
