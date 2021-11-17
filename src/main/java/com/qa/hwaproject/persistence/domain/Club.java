9ef57e680f7b6ac7b637c5d4dec715adb0b6e6c0
unknown <alzein7@outlook.com>
2021-11-17 12:58:35 +0000

    HWA-17 #comment this is my club domain java file added with the club name field included

--- src/main/java/com/qa/hwaproject/persistence/domain/Club.java
+++ src/main/java/com/qa/hwaproject/persistence/domain/Club.java
@@ -1,5 +1,38 @@
 package com.qa.hwaproject.persistence.domain;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Club {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@Column(name = "club_name", unique = true, nullable = false)
+	private String clubName;
+	
+	@Column(name = "club_league", nullable = false)
+	private String clubLeague;
+	
+	@Column(name = "club_location", nullable = false)
+	private String clubLocation;
+	
+	@Column(name = "club_stadium", nullable = false)
+	private String clubStadium;
+	
+	@Column(name = "club_value", nullable = false)
+	private double clubValue;
 
 }
