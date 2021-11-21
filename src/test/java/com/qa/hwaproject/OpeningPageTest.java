cf794ddaf7c25fe5f21c90e5c0cbb47111bb74be
unknown <alzein7@outlook.com>
2021-11-21 17:16:05 +0000

    HWA-22 #comment added user acceptance selenium openingpage test

---
+++ src/test/java/com/qa/hwaproject/OpeningPageTest.java
@@ -0,0 +1,36 @@
+package com.qa.hwaproject;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class OpeningPageTest {
+	
+	private WebDriver driver;
+	
+	@Before
+	public void setup() {
+	System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
+	driver = new ChromeDriver();
+	driver.manage().window().setSize(new Dimension(1366, 768));
+	}
+	
+	@Test
+	public void test() throws InterruptedException {
+		driver.get("http://127.0.0.1:5500/HTML/index.html");
+		
+		assertEquals("http://127.0.0.1:5500/HTML/index.html", driver.getCurrentUrl());
+		System.out.println(driver.getCurrentUrl());
+	}
+	
+	@After
+	public void tearDown() {
+		driver.close();
+	}
+
+}
