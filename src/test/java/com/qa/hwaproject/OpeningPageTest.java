c9d7be8f77c5e409d2e79ee56d52c9852cc0d769
unknown <alzein7@outlook.com>
2021-11-22 15:59:17 +0000

    HWA-1 #comment modified openingpagetest file

--- src/test/java/com/qa/hwaproject/OpeningPageTest.java
+++ src/test/java/com/qa/hwaproject/OpeningPageTest.java
@@ -13,24 +13,24 @@ public class OpeningPageTest {
 	
 	private WebDriver driver;
 	
-	@Before
-	public void setup() {
-	System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
-	driver = new ChromeDriver();
-	driver.manage().window().setSize(new Dimension(1366, 768));
-	}
-	
-	@Test
-	public void test() throws InterruptedException {
-		driver.get("http://127.0.0.1:5500/HTML/index.html");
-		
-		assertEquals("http://127.0.0.1:5500/HTML/index.html", driver.getCurrentUrl());
-		System.out.println(driver.getCurrentUrl());
-	}
-	
-	@After
-	public void tearDown() {
-		driver.close();
-	}
+//	@Before
+//	public void setup() {
+//	System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
+//	driver = new ChromeDriver();
+//	driver.manage().window().setSize(new Dimension(1366, 768));
+//	}
+//	
+//	@Test
+//	public void test() throws InterruptedException {
+//		driver.get("http://127.0.0.1:5500/HTML/index.html");
+//		
+//		assertEquals("http://127.0.0.1:5500/HTML/index.html", driver.getCurrentUrl());
+//		System.out.println(driver.getCurrentUrl());
+//	}
+//	
+//	@After
+//	public void tearDown() {
+//		driver.close();
+//	}
 
 }
