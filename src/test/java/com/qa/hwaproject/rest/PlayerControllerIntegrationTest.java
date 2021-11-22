5aabc3aa5d2355e025053e1464a6f6ac339d61f2
unknown <alzein7@outlook.com>
2021-11-22 16:38:16 +0000

    HWA-1 #comment modified pom.xml file

--- src/test/java/com/qa/hwaproject/rest/PlayerControllerIntegrationTest.java
+++ src/test/java/com/qa/hwaproject/rest/PlayerControllerIntegrationTest.java
@@ -4,7 +4,6 @@ package com.qa.hwaproject.rest;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
